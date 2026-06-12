35fbcdc1dcd5e85cc3a9ee7268b5cf500506a6a1
wallace-araujo01 <wallace.araujo01@fatec.sp.gov.br>
2026-06-12 19:51:25 -0300

    Refactor Usuario class with constructor and methods

--- Usuario.java
+++ Usuario.java
@@ -3,25 +3,28 @@ public class Usuario {
     private String nome;
     private String email;
 
-    public int getMatricula() {
-        return matricula;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public String getEmail() {
-        return email;
-    } 
-    public void setEmail(String email) {
+    public Usuario(int matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
         this.email = email;
+    }
+
+    public int getMatricula() { return matricula; }
+    public String getNome()   { return nome; }
+    public String getEmail()  { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Usuario[" + matricula + "] " + nome + " <" + email + ">";
     }
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-    @Override
-    public String toString() {
-        return "[" + matricula + "] " + nome + " - " + email;
+        if (this == obj) return true;
+        if (!(obj instanceof Usuario)) return false;
+        Usuario outro = (Usuario) obj;
+        return this.matricula == outro.matricula;
     }
 }
