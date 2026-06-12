1d417765ea0cc6393d1a4961de8eb026779c7323
wallace-araujo01 <wallace.araujo01@fatec.sp.gov.br>
2026-06-12 19:49:10 -0300

    Implementar lista dupla com métodos de inserção e remoção

--- ListaDupla.java
+++ ListaDupla.java
@@ -1,3 +1,106 @@
 public class ListaDupla {
-    
+    private NoDuplo primeiro;
+    private NoDuplo ultimo;
+    private int tamanho;
+
+    public ListaDupla() {
+        this.primeiro = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+    }
+
+    public void insereInicio(Livro livro) {
+        NoDuplo novo = new NoDuplo(livro);
+        if (primeiro == null) {
+            primeiro = novo;
+            ultimo = novo;
+        } else {
+            novo.setProximo(primeiro);
+            primeiro.setAnterior(novo);
+            primeiro = novo;
+        }
+        tamanho++;
+    }
+
+    public void insereFim(Livro livro) {
+        NoDuplo novo = new NoDuplo(livro);
+        if (ultimo == null) {
+            primeiro = novo;
+            ultimo = novo;
+        } else {
+            novo.setAnterior(ultimo);
+            ultimo.setProximo(novo);
+            ultimo = novo;
+        }
+        tamanho++;
+    }
+
+    public Livro removePrimeiro() {
+        if (primeiro == null) return null;
+        Livro info = primeiro.getInfo();
+        primeiro = primeiro.getProximo();
+        if (primeiro != null) {
+            primeiro.setAnterior(null);
+        } else {
+            ultimo = null;
+        }
+        tamanho--;
+        return info;
+    }
+
+    public Livro removeUltimo() {
+        if (ultimo == null) return null;
+        Livro info = ultimo.getInfo();
+        ultimo = ultimo.getAnterior();
+        if (ultimo != null) {
+            ultimo.setProximo(null);
+        } else {
+            primeiro = null;
+        }
+        tamanho--;
+        return info;
+    }
+
+    public Livro buscarPorIsbn(String isbn) {
+        NoDuplo atual = primeiro;
+        while (atual != null) {
+            if (atual.getInfo().getIsbn().equals(isbn)) {
+                return atual.getInfo();
+            }
+            atual = atual.getProximo();
+        }
+        return null;
+    }
+
+    public void listarDoInicio() {
+        if (primeiro == null) {
+            System.out.println("  [Lista vazia]");
+            return;
+        }
+        NoDuplo atual = primeiro;
+        while (atual != null) {
+            System.out.println("  " + atual.getInfo());
+            atual = atual.getProximo();
+        }
+    }
+
+    public void listarDoFim() {
+        if (ultimo == null) {
+            System.out.println("  [Lista vazia]");
+            return;
+        }
+        NoDuplo atual = ultimo;
+        while (atual != null) {
+            System.out.println("  " + atual.getInfo());
+            atual = atual.getAnterior();
+        }
+    }
+
+    public int tamanho() {
+        return tamanho;
+    }
+
+    public boolean estaVazia() {
+        return tamanho == 0;
+    }
 }
