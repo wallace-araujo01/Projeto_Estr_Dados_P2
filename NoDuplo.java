60d917928d2778021d9b28620f9fac8527b9e863
wallace-araujo01 <wallace.araujo01@fatec.sp.gov.br>
2026-06-12 19:50:52 -0300

    Adicionando classe NoDuplo

--- NoDuplo.java
+++ NoDuplo.java
@@ -1,3 +1,19 @@
 public class NoDuplo {
-    
+    private Livro info;
+    private NoDuplo proximo;
+    private NoDuplo anterior;
+
+    public NoDuplo(Livro info) {
+        this.info = info;
+        this.proximo = null;
+        this.anterior = null;
+    }
+
+    public Livro getInfo()         { return info; }
+    public NoDuplo getProximo()    { return proximo; }
+    public NoDuplo getAnterior()   { return anterior; }
+
+    public void setInfo(Livro info)          { this.info = info; }
+    public void setProximo(NoDuplo proximo)  { this.proximo = proximo; }
+    public void setAnterior(NoDuplo anterior){ this.anterior = anterior; }
 }
